9c8ae5206f783de9ca4e3c5804667422a4ce282f
Suruchi <suruchi.singla@oracle.com>
2024-07-08 23:51:44 +0530

    v2024.3.2

--- modules/iam-group/data.tf
+++ modules/iam-group/data.tf
@@ -1,16 +1,12 @@
 // Copyright (c) 2021, 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
-#############################
-## Data Block - Identity
-# Create Users
-#############################
+data "oci_identity_users" "users" {
 
-
-data "oci_identity_groups" "iam_groups" {
-  #Required
   compartment_id = var.tenancy_ocid
-
-  #Optional
-  name = var.group_name
 }
+
+output "users_details" {
+  value = data.oci_identity_users.users
+}
+
---
+++ modules/iam-group/main.tf
@@ -0,0 +1,44 @@
+// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+############################
+# Resource Block - Identity
+# Create Groups
+############################
+
+resource "oci_identity_group" "group" {
+  count = (var.matching_rule != "" && var.matching_rule != null) ? 0 : 1
+
+  #Required
+  compartment_id = var.tenancy_ocid
+  description    = var.group_description
+  name           = var.group_name
+
+  #Optional
+  defined_tags  = var.defined_tags
+  freeform_tags = var.freeform_tags
+
+}
+
+############################
+# Resource Block - Identity
+# Create Dynamic Groups
+############################
+
+resource "oci_identity_dynamic_group" "dynamic_group" {
+  count = (var.matching_rule != "" && var.matching_rule != null) ? 1 : 0
+
+  #Required
+  compartment_id = var.tenancy_ocid
+  description    = var.group_description
+  matching_rule  = var.matching_rule
+  name           = var.group_name
+
+  #Optional
+  defined_tags  = var.defined_tags
+  freeform_tags = var.freeform_tags
+
+  lifecycle {
+    ignore_changes = [defined_tags["Oracle-Tags.CreatedOn"], defined_tags["Oracle-Tags.CreatedBy"]]
+  }
+}
