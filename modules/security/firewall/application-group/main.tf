ca162e9f3caec823e91f27c47cd3dc9d6864d6d2
Suruchi <suruchi.singla@oracle.com>
2024-08-06 10:57:33 +0530

    Delete modules/security/firewall directory

--- modules/security/firewall/application-group/main.tf
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-#
-resource "oci_network_firewall_network_firewall_policy_application_group" "network_firewall_policy_application_group" {
-    #Required
-    apps = var.apps
-    name = var.app_group_name
-    network_firewall_policy_id = var.network_firewall_policy_id
-}
--- modules/security/firewall/service-list/main.tf
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2021, 2022, Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-#
-resource "oci_network_firewall_network_firewall_policy_service_list"  "network_firewall_policy_service_list" {
-  name                       = var.service_list_name
-  network_firewall_policy_id = var.network_firewall_policy_id
-  services                   = var.services
-  #services                   = var.services != null ? (local.services == null ? ["INVALID SERVICE"] : local.services) : null
-}
